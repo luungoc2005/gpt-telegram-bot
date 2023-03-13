445cddfd33b07588a3b206ca724e9a8e2a55060a
luungoc2005 <luungoc2005@gmail.com>
2023-03-14 02:26:44 +0800

    refactor chat history

--- backend/model.py
+++ backend/model.py
@@ -19,16 +19,16 @@ ENABLE_RANKER = True
 
 # Constants
 CACHE_DIR = "G:\\Projects\\Samples\\tmp"
-INITIAL_PROMPT = """{{bot_name}} is a very smart girl
-{{bot_name}} and {{first_name}} are dating
-This is a recent discussion between {{first_name}} and {{bot_name}}
----
-{{first_name}}: Hello
-{{bot_name}}: Good {{part_of_day}}"""
+INITIAL_PROMPT = """{{bot_name}} is an assistant chatbot
+{{bot_name}} can engage in discussions and answer questions on a wide range of topics
+
+{{bot_name}}: Good {{part_of_day}}
+{{chat_history}}"""
 # INITIAL_PROMPT = """{{bot_name}} is a large language model. It can talk about anything.
 
 # {{first_name}}: Hello
-# {{bot_name}}: Good morning"""
+# {{bot_name}}: Good morning
+# {{chat_history}}"""
 RANKER_MODELS = [
     # ('microsoft/DialogRPT-human-vs-rand', .5, 0),
     # ('microsoft/DialogRPT-human-vs-machine', .5, 0),
@@ -53,12 +53,13 @@ def get_part_of_day():
         return "evening"
 
 
-def get_initial_prompt(first_name):
+def get_initial_prompt(first_name, chat_history):
     BOT_NAME = "Sydney"
     prompt = INITIAL_PROMPT \
         .replace("{{first_name}}", first_name) \
         .replace("{{bot_name}}", BOT_NAME) \
-        .replace("{{part_of_day}}", get_part_of_day())
+        .replace("{{part_of_day}}", get_part_of_day()) \
+        .replace("{{chat_history}}", chat_history)
     bot_prefix = f"{BOT_NAME}: "
     human_prefix = f"{first_name}: "
 
@@ -165,24 +166,31 @@ def get_model():
         args = {
             "cache_dir": CACHE_DIR,
         }
+        LOAD_IN_8BIT = False
         if torch.cuda.is_available:
             args["torch_dtype"] = torch.float16
 
-        if os.path.exists(checkpoint_device_map_name):
-            with open(checkpoint_device_map_name) as f:
-                device_map = json.load(f)
-        else:
-            with init_empty_weights():
-                model = AutoModelForCausalLM.from_pretrained(checkpoint, **args)
-            
-            device_map = infer_auto_device_map(model,
-                max_memory=MAX_MEMORY)
+            if LOAD_IN_8BIT:
+                args["load_in_8bit"] = True
+                args["device_map"] = "auto"
+                args["max_memory"] = MAX_MEMORY
 
-            print(device_map)
-            with open(checkpoint_device_map_name, "w") as f:
-                json.dump(device_map, f)
+            else:
+                if os.path.exists(checkpoint_device_map_name):
+                    with open(checkpoint_device_map_name) as f:
+                        device_map = json.load(f)
+                else:
+                    with init_empty_weights():
+                        model = AutoModelForCausalLM.from_pretrained(checkpoint, **args)
+                    
+                    device_map = infer_auto_device_map(model,
+                        max_memory=MAX_MEMORY)
 
-        args["device_map"] = device_map
+                    print(device_map)
+                    with open(checkpoint_device_map_name, "w") as f:
+                        json.dump(device_map, f)
+
+                args["device_map"] = device_map
         model = AutoModelForCausalLM.from_pretrained(checkpoint, **args)
     model.eval()
 
@@ -191,27 +199,33 @@ def get_model():
     return model, tokenizer, device
 
 
+def build_chat_history(history_items, human_prefix, bot_prefix):
+    chat_history = []
+    for item in history_items:
+        if item['message'] == "/reset":
+            chat_history = []
+            continue
+        elif item['message'].startswith("/"):
+            continue
+
+        if int(item['from']) == 0:
+            chat_history.append(human_prefix + item['message'])
+        else:
+            chat_history.append(bot_prefix + item['message'])
+    return '\n'.join(chat_history)
+
+
 @torch.no_grad()
 def predict(model, tokenizer, device, first_name, history_items, message):
     num_return_sequences=3
     num_beams = num_return_sequences * 2
     # Prepare input
-    initial_prompt, bot_prefix, human_prefix = get_initial_prompt(first_name)
+    chat_history = build_chat_history(history_items, human_prefix, bot_prefix)
+    initial_prompt, bot_prefix, human_prefix = get_initial_prompt(first_name, chat_history)
     prompt = initial_prompt
     rank_context = message
 
-    for item in history_items:
-        if item['message'] == "/reset":
-            prompt = initial_prompt
-            continue
-        elif item['message'].startswith("/"):
-            continue
-
-        prompt += f"\n"
-        if int(item['from']) == 0:
-            prompt += human_prefix + item['message']
-        else:
-            prompt += bot_prefix + item['message']
+    prompt = initial_prompt
     prompt += "\n" + human_prefix + message
     prompt += "\n" + bot_prefix
 
@@ -227,11 +241,11 @@ def predict(model, tokenizer, device, first_name, history_items, message):
         max_new_tokens=64,
         top_k=50,
         top_p=0.8,
-        temperature=0.75,
+        temperature=0.8,
         repetition_penalty=2.0,
         # diversity_penalty=2.0,
         no_repeat_ngram_size=2,
-        length_penalty=1.0,
+        length_penalty=2.0,
         num_beams=num_beams,
         # num_beam_groups=num_return_sequences,
         do_sample=True,
