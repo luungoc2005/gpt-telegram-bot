4776498207164ec9a08b67c20355d3dbac14276d
Ngoc Nguyen <luungoc2005@gmail.com>
2023-03-15 01:12:18 +0800

    make llama work with llama.cpp main

--- backend/llama.py
+++ backend/llama.py
@@ -8,10 +8,10 @@ handler = logging.FileHandler('llama.log')
 handler.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
-CPP_PATH = getenv('CPP_PATH')
-LLAMA_PATH = getenv('LLAMA_PATH')
-
 def llama_generate(prompt, *args, **kwargs):
+    CPP_PATH = getenv('CPP_PATH')
+    LLAMA_PATH = getenv('LLAMA_PATH')
+
     try:
         output = subprocess.check_output([
             CPP_PATH,
@@ -23,10 +23,17 @@ def llama_generate(prompt, *args, **kwargs):
             "--top_k", str(40),
             "--top_p", str(0.73),
             "--repeat_last_n", str(64),
-            "--repeat_penalty", str(1.3),
-            "-q",
-        ], encoding='utf-8')
+            "--repeat_penalty", str(1),
+        ], 
+        encoding='utf-8',
+        stderr=subprocess.DEVNULL)
         logger.debug("Raw LLAMA output: " + output)
         return [output]
     except Exception as e:
         logger.error(e)
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    print(llama_generate("Hello world"))
